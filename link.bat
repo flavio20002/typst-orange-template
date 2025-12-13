faf46b78b00a0f85a56704ccef649a6fd1634e27
Flavio <flvb86@gmail.com>
2025-12-13 10:32:12 +0100

    Update version

--- link.bat
+++ link.bat
@@ -1,2 +1,2 @@
 mkdir %LocalAppData%\typst\packages\local\orange-book
-mklink /D %LocalAppData%\typst\packages\local\orange-book\0.6.2 %cd%
+mklink /D %LocalAppData%\typst\packages\local\orange-book\0.7.0 %cd%
